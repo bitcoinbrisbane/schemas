30bcbfb2c1080ed24999c0a339f9636cdc5e5d03
Lucas Cullen <bitcoinbrisbane@users.noreply.github.com>
2021-11-25 08:32:12 +1000

    Update deploy.sh
    Add bash shebang

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 aws s3 sync schemas s3://schemas.testnet.verida.io --content-type application/schema+json --profile verida-testnet --delete
 aws cloudfront create-invalidation --profile verida-testnet --distribution-id E2DU6UUUVFKZWK --paths "/*"
